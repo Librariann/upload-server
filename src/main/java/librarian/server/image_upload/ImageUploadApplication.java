137377481510d29ea4788c78ad3d715b2845164a
Librariann <okpc0306@naver.com>
2025-01-26 09:00:00 +0900

    Disable database auto-configuration for storage-only application

--- src/main/java/librarian/server/image_upload/ImageUploadApplication.java
+++ src/main/java/librarian/server/image_upload/ImageUploadApplication.java
@@ -2,8 +2,9 @@ package librarian.server.image_upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ImageUploadApplication {
 
 	public static void main(String[] args) {
